bcdeb86c275ceea69f1e0c5ef8651d8697f9114c
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2020-07-27 23:35:57 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,17 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-    @message.save
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
   end
 
   private
