0ca6361600fe8c32ee795ca00b0d60dc832692d3
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2020-07-27 22:56:55 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
