250db7ae4890b2248a93290b95075035cd704e43
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2020-07-27 19:00:34 +0900

    フロント実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # こちらは削除してOKです
+  root "messages#index"
 end
