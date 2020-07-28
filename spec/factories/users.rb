f5e3029383cf6db582651b639bcfc14a8bfe8876
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2020-07-28 15:35:42 +0900

    各モデルのテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 8)
+    password {password}
+    password_confirmation {password}
+  end
+end
